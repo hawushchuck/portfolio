9e9e2294348fc14ff9a9c1d93c60810c0426b49c
hawushchuck <123550338+hawushchuck@users.noreply.github.com>
2026-02-07 22:44:20 +0000

    Aniahaluszczak.js

--- Aniahaluszczak.js
+++ Aniahaluszczak.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const projectDetails = {
     1: {
       title: "Simbioze 2.0: Movie poster", 
-      description: "A film poster for a documentary about an art event. I made this using my photography and scans of medical documents, Inspired by the story of ambition and sacrifice in the film, as well as the softness of art and nature. The film is currently being submitted to festivals.",
+      description: "A film poster for a documentary about an art event. I made this using my photography and scans of medical documents, inspired by the story of ambition and sacrifice in the film, as well as the softness of art and nature. The film is currently being submitted to festivals.",
       images: ["images/movie poster Large.jpeg","images/lungs.jpeg","images/landscape.jpg"],
       videos: [
         { type: "instagram", url: "https://www.instagram.com/reel/DQhG3iQiEg2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="}
@@ -15,7 +15,7 @@ const projectDetails = {
     },
     2: {
       title: "Simbioze 2.0: Riga exhibition", 
-      description: "Simbioze 2.0 was an outdoor plein air held in July 2025, then exhibited in Riga November 2025. The poster was featured in two publications and this event featured over 50 art works including performances and fashion pieces. This shows the process of the exhibition poster design; combining printmaking featuring natural elements with art from city streets. Inspired by the idea of a portal, as well as the circularity in art and nature. Check the links to read about the Exhibition. ",
+      description: "Simbioze 2.0 was an outdoor plein air held in July 2025, then exhibited in Riga in November 2025. The poster was featured in two publications, and this event featured over 50 artworks, including performances and fashion pieces. This shows the process of the exhibition poster design; combining printmaking featuring natural elements with art from city streets. Inspired by the idea of a portal, as well as the circularity in art and nature. Check the links to read about the Exhibition. ",
       link: "https://satori.lv/article/skujene-tiks-atklata-brivdabas-laikmetigas-makslas-izstade-simbioze-20",
       images: ["images/veldze poster.jpg","images/EXP-ezgif.com-optimize.gif","images/-93.jpeg","images/745098A1-5DCF-4320-82A7-DC2EC9429BC0.jpeg","images/draft1.jpeg","images/Envelope Large.jpeg"],
       videos: [
@@ -25,7 +25,7 @@ const projectDetails = {
     },
     3: {
       title: "Simbioze 2.0: Skujene Plein air",
-      description: "Simbioze 2.0 was an outdoor plein air held in July 2025, The event featured 50 artists and was notably sponsored by The European Union, KKF Latvia, Red Bull and Bourzma studio among others. This shows the process of the poster design, inspired by the idea of forging a path, and uses paper cut and collage. I designed the logo using an experimental process giving reference to Kefir - the name of the studio. I have included and some event photos for context.",
+      description: "Simbioze 2.0 was an outdoor plein air held in July 2025, The event featured 50 artists and was notably sponsored by The European Union, KKF Latvia, Red Bull and Bourzma studio among others. This shows the process of the poster design, inspired by the idea of forging a path, and uses paper cut and collage. I designed the logo using an experimental process giving reference to Kefir - the name of the studio. I have included some event photos for context.",
       link: "https://satori.lv/article/skujene-tiks-atklata-brivdabas-laikmetigas-makslas-izstade-simbioze-20",
       images: ["images/PHOTO-2025-07-23-21-37-02.jpg","images/simbiozeposter.jpeg","images/IMG_8850.jpeg","images/OG Scan.jpeg","images/sposter b_w.png", "images/INVITES ENG 2.jpeg","images/Screenshot 2025-09-19 at 16.23.20.jpeg"],
       videos: [
@@ -35,7 +35,7 @@ const projectDetails = {
     },
     4: {
       title: "Open Call",
-      description: "I designed an Open Call poster for Simbioze 2.0, an outdoor plein air event held in July 2025 in Skujene, Latvia. I created an animation to increase engagement on Instagram stories. We recieved over 100 aplications in 1 month of running the advert, and were able to use the responses to tailor workshops and offer initial advice.",
+      description: "I designed an Open Call poster for Simbioze 2.0, an outdoor plein air event held in July 2025 in Skujene, Latvia. I created an animation to increase engagement on Instagram stories. We received over 100 applications in 1 month of running the advert, and were able to use the responses to tailor workshops and offer initial advice.",
       images: ["images/2-3 Large.jpeg", "images/open call final Large.jpeg","images/Sequence01-ezgif.com-resize.gif"],
        videos: [
         { type: "instagram", url: "https://www.instagram.com/p/DJR3RoRieRY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" }
@@ -52,7 +52,7 @@ const projectDetails = {
     },
     6: {
       title: "British Business Bank Graphic Design",
-      description: "During my internship at British Business Bank (a government funded bank), I took on several projects around making finance more acessible to a range of audiences using graphic, illustration and motion design skills",
+      description: "During my internship at British Business Bank (a government-funded bank), I took on several projects around making finance more accessible to a range of audiences using graphic, illustration and motion design skills",
       images: ["images/SBET factsheet  Large.jpeg","images/SBET factsheet 2 Large.jpeg","images/SBET factsheet 3 Large.jpeg","images/SBET factsheet 4 Large.jpeg","images/Icons for the intranet bbb 08045-08068 Large.jpeg","images/v7pngs-02 Large.jpeg","images/v7pngs-05 Large.jpeg","images/SUL Ambassadors colourways 24_25 Large.jpeg","images/external 16-9_BBB 08520 Woof The Yorkshire Dog Shop Large.jpeg"],
       videos: [
         { type: "youtube", url: "https://youtube.com/shorts/j_IWuJWzhw4?feature=share" }
@@ -62,7 +62,7 @@ const projectDetails = {
       title: "Post Produzione",
       description: "Postproduzione is an Italian podcast that discusses visual art through a youthful, contemporary lens. This shows the final design in context and some initial idea development.",
       link: "https://open.spotify.com/episode/4Z0nLIKZqIJgT2xazTw3QO?si=e413eabe7aa7450e",
-      images: ["images/PostProduzione_colori.jpeg","images/image-294f887d-fe32-45e2-8e19-5d7fe3fe9112.png","images/image-e47eb326-529f-457b-b2d5-b0fa9c556dbe.png","images/Post Prozuzione_8.jpeg","images/Post Produzione_4 Large.jpeg"],
+      images: ["images/PostProduzione_colori.jpeg","images/image-294f887d-fe32-45e2-8e19-5d7fe3fe9112.png","images/image-e47eb326-529f-457b-b2d5-b0fa9c556dbe.png","images/Post Prozuzione_8.jpeg","images/Post Produzione_4 Large.jpeg","PP Large.jpeg"],
     },
     8: {
       title: "OFN Film Festival ",
@@ -71,7 +71,7 @@ const projectDetails = {
     },
     9: {
       title: "Grove Website and Branding",
-      description: "Grove Dyslexia is a small business that provides diagnostic dyslexia assessments for children and adults. I redesigned the website using Figma and Square Space with a focus on accessibility. It was also an aim to design something that was more reflective of the growth of the business, and the refinement of it’s services.",
+      description: "Grove Dyslexia is a small business that provides diagnostic dyslexia assessments for children and adults. I redesigned the website using Figma and Square Space with a focus on accessibility. It was also an aim to design something more reflective of the growth of the business, and the refinement of it’s services.",
       link: "https://grovedyslexia.squarespace.com/",
       images: ["images/9C4B7BE4-927E-411E-99DB-C9F4DB6910FD Medium.jpeg","images/Screenshot 2025-06-23 at 21.57.41 Large.jpeg","images/Capture-2025-06-23-215646 Large.jpeg","images/grove.png","images/grove 2.png"]
     },
